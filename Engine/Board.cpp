f7f5f6d64d9790b842de44d6768cb5057aff32f0
Luka Burjanadze <lukakiko123z@gmail.com>
2026-01-15 00:49:43 +0400

    deleted color from constructor and instead added it in DrawCell function

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -1,14 +1,13 @@
 #include "Board.h"
 
-Board::Board(int in_width, int in_height, Color in_color)
+Board::Board(int in_width, int in_height)
 	:
 	width(in_width),
-	height(in_height),
-	c(in_color)
+	height(in_height)
 {
 }
 
-void Board::DrawCell(const Vei2& gridPos, Graphics& gfx)
+void Board::DrawCell(const Vei2& gridPos, Graphics& gfx, Color c)
 {
 	gfx.DrawRectDim(gridPos * cellSize, cellSize, cellSize, c);
 }
--- Engine/Board.h
+++ Engine/Board.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include "Graphics.h"
-#include "Colors.h"
 #include "Vei2.h"
+#include "Colors.h"
 
 class Board
 {
 public:
-	Board(int in_width, int in_height, Color in_color);
-	void DrawCell(const Vei2& gridPos, Graphics& gfx);
+	Board(int in_width, int in_height);
+	void DrawCell(const Vei2& gridPos, Graphics& gfx, Color c);
 private:
 	Color c;
 	static constexpr int cellSize = 15;
