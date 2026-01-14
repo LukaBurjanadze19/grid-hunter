9f6707d3798a8b8f889a5a040fccd36a4f74526d
Luka Burjanadze <lukakiko123z@gmail.com>
2026-01-15 01:13:54 +0400

    added addition operator overloading for Vei2 class

--- Engine/Vei2.cpp
+++ Engine/Vei2.cpp
@@ -7,6 +7,13 @@ Vei2::Vei2(int x, int y)
 {
 }
 
+Vei2& Vei2::operator+=(const Vei2& rhs)
+{
+    x += rhs.x;
+    y += rhs.y;
+    return *this;
+}
+
 const Vei2 Vei2::operator*(int val) const
 {
     return Vei2(x * val, y * val);
--- Engine/Vei2.h
+++ Engine/Vei2.h
@@ -5,6 +5,7 @@ class Vei2
 public:
 	Vei2() = default;
 	Vei2(int x, int y);
+	Vei2& operator += (const Vei2& rhs);
 	const Vei2 operator * (int val) const;
 public:
 	int x;
